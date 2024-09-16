57b5a939e1d005723421b35360108cf859a40b79
loftiestspore2 <logphs@gmail.com>
2024-09-16 20:40:25 +0200

    Create ChessB.java

---
+++ Chess/ChessB.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/**
+ * created by Lofttiestspore2 05/09
+ */
+public class ChessB {
+    public static void main(String[] args) {
+
+    }
+}
