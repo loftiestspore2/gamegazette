07f09f7853e73ce50529cfe36bad608384a51917
loftiestspore2 <logphs@gmail.com>
2024-10-05 05:31:38 +0200

    added to Pawn and board class

--- Chess/gui/Table.java
+++ Chess/gui/Table.java
@@ -2,6 +2,8 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Table {
 
@@ -9,10 +11,31 @@ public class Table {
     private static Dimension mainFrame = new Dimension(600,600);
 
     public Table(){
-        this.gameFrame=new JFrame("gamegazette_Chess");
-
+        this.gameFrame=new JFrame("Gamegazette Chess");
+        final JMenuBar tMenuBar = new JMenuBar();
+        menuMeth(tMenuBar);
         this.gameFrame.setSize(mainFrame);
         this.gameFrame.setVisible(true);
+    }
+
+    private void menuMeth(final JMenuBar tMenuBar){
+        tMenuBar.add(createFileMenu());
+    }
+
+    private JMenu createFileMenu(){ // to load Moves
+        final JMenu fMenu = new JMenu("File");
+
+        final JMenuItem openPGN = new JMenuItem("Load PGN File");
+
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("open pgn file");
+            }
+        });
+        fMenu.add(openPGN);
+        return fMenu;
     }
 
 }
